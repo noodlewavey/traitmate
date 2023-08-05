bccfdbc9215a7d3723f32287c6444401e06caccd
noodlewavey <117111580+noodlewavey@users.noreply.github.com>
2023-08-05 16:17:08 -0400

    added typography settings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 import logo from './logo.svg';
-import './App.css';
 import CenteredBox from './CenteredBox';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import './index.css';
 import ProfileTemplate from './ProfileTemplate';
+import {CssBaseline} from '@mui/material';
+import './styles.css';
+
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#dfd3bc',
+    },
+    text: {
+      primary: 'black',
+    },
+
+    accent: {
+      main: '#002fff',
+      accent1: '#0018bc',
+      accent2: '#0a2381',
+      accent3: '#00114d',
+      accent4: 'black',
+    }
+  },
+  typography: {
+    fontFamily: 'IBM Plex Mono, Display Regular, Display Italic, Display Light Italic, Display Bold Italic, Display Bold, monospace',
+    body1: {
+      fontFamily: 'IBM Plex Mono, monospace',
+      fontWeight: 400,
+    },
+    body2: {
+      fontFamily: 'IBM Plex Mono, monospace',
+      fontWeight: 300,
+    },
+    h1: {
+      fontFamily:'DisplayLightItalic, serif',
+    },
+    h2:{
+      fontFamily: 'DisplayItalic, serif',
+    },
+    button: {
+      fontFamily: 'IBM Plex Mono, monospace',
+      fontWeight: 600,
+    }
+  },
+});
 
 function App() {
+
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <ProfileTemplate />
-    </div>
+      </ThemeProvider>
   );
 }
 
--- src/ProfileTemplate.js
+++ src/ProfileTemplate.js
@@ -1,9 +1,12 @@
 import CenteredBox from "./CenteredBox";
+import { Typography } from "@mui/material";
 
 const ProfileTemplate = () => {
     return (
         <CenteredBox>
-            <p>Hello</p>
+            <Typography variant = "h1">Hello</Typography>
+            <Typography variant ="body1">Lalala</Typography>"
+            <Typography variant="body2">LAla</Typography>
         </CenteredBox>
     );
   };
