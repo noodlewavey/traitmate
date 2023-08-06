1f1b68b8086e7722f140ccfacdf8396a9d4ea6ec
noodlewavey <117111580+noodlewavey@users.noreply.github.com>
2023-08-06 19:42:29 -0400

    last working version of profile card

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import './index.css';
 import ProfileTemplate from './ProfileTemplate';
 import {CssBaseline} from '@mui/material';
 import './styles.css';
-
-
+import IconButton from '@mui/material/IconButton';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { Delete } from '@mui/icons-material';
+import Box from '@mui/material/Box';
 
 const theme = createTheme({
   palette: {
@@ -54,7 +57,15 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <ProfileTemplate />
-      </ThemeProvider>
+      <Box sx={{display: 'flex', justifyContent:'space-between', alignItems: 'center', width: '100%', padding: '0 1rem'}}>
+      <IconButton aria-label="dislike">
+        <DeleteForeverIcon />
+      </IconButton>
+      <IconButton aria-label="like">
+        <FavoriteBorderIcon/>
+      </IconButton>
+      </Box>
+    </ThemeProvider>
   );
 }
 
