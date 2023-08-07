4b9adb8ae133ba7d562c4c835b379a28651f8c96
noodlewavey <117111580+noodlewavey@users.noreply.github.com>
2023-08-06 20:04:34 -0400

    added side buttons

--- src/App.js
+++ src/App.js
@@ -52,21 +52,47 @@ const theme = createTheme({
 });
 
 function App() {
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <ProfileTemplate />
-      <Box sx={{display: 'flex', justifyContent:'space-between', alignItems: 'center', width: '100%', padding: '0 1rem'}}>
-      <IconButton aria-label="dislike">
-        <DeleteForeverIcon />
-      </IconButton>
-      <IconButton aria-label="like">
-        <FavoriteBorderIcon/>
-      </IconButton>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',     // added to stack items vertically
+          justifyContent: 'center',    // center items vertically
+          alignItems: 'stretch',        // center items horizontally
+          height: '100vh',             // full viewport height
+          //padding: '0 1rem',
+          position: 'relative',        // relative positioning context for the buttons
+        }}
+      >
+        <ProfileTemplate sx={{width: '100%', maxWidth: 'none'}}/>
+        <IconButton 
+          aria-label="dislike" 
+          sx={{ 
+            position: 'absolute',          // absolute positioning
+            top: '60%',                    // half the height of the viewport
+            left: '1rem',                  // 1rem from the left edge
+            transform: 'translateY(-50%)'  // vertical centering
+          }}
+        >
+          <DeleteForeverIcon style={{ fontSize: '6rem' }}/>
+        </IconButton>
+        <IconButton 
+          aria-label="like" 
+          sx={{ 
+            position: 'absolute',          // absolute positioning
+            top: '60%',                    // half the height of the viewport
+            right: '1rem',                 // 1rem from the right edge
+            transform: 'translateY(-50%)'  // vertical centering
+          }}
+        >
+          <FavoriteBorderIcon style={{ fontSize: '6rem' }}/>
+        </IconButton>
       </Box>
     </ThemeProvider>
   );
 }
+
 
 export default App;
