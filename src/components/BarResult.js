b35dc80f8ff9c7aaa0dc82bfbb4a2657e3542a9f
noodlewavey <117111580+noodlewavey@users.noreply.github.com>
2023-08-18 19:10:22 -0400

    non-working chart.js

--- src/components/BarResult.js
+++ src/components/BarResult.js
@@ -1,17 +1,28 @@
-import React from "react";
-import Chart from 'chart.js';
+import React, { useEffect, useRef } from "react";
+import { Chart } from 'chart.js';
 
+const BarChart = () => {
+  const chartRef = useRef(null);
 
-const ctx = document.getElementById('chart_id').getContext('2d');
-const BarResult = new Chart(ctx, {
-      type: 'bar',
-      data: {
+  useEffect(() => {
+    if (chartRef.current) {
+      const ctx = chartRef.current.getContext('2d');
+      
+      new Chart(ctx, {
+        type: 'bar',
+        data: {
           labels: ['Red', 'Blue', 'Yellow', 'Green', 'Orange'],
           datasets: [{
-              label: 'Different colors bars',
-              data: [90, 100, 80, 50, 70],
-              backgroundColor: ["#ed1e1e", "#2269e5", "#fce302", "#04f759", "#fc9700"]
+            label: 'Different colors bars',
+            data: [90, 100, 80, 50, 70],
+            backgroundColor: ["#ed1e1e", "#2269e5", "#fce302", "#04f759", "#fc9700"]
           }]
-      }
-  });
-export default BarResult;
+        }
+      });
+    }
+  }, []);
+
+  return <canvas id="chart_id" ref={chartRef}></canvas>;
+}
+
+export default BarChart;
