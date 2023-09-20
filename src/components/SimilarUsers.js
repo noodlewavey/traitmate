0bafa486e3cc4b20adb37ce9bc65ce0bcdb265ea
noodlewavey <117111580+noodlewavey@users.noreply.github.com>
2023-09-19 20:29:29 -0400

    implemented go to next user! now need to do swipe or framer motion animation...later

--- src/components/SimilarUsers.js
+++ src/components/SimilarUsers.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ProfileTemplate from './ProfileTemplate';
+import { Box } from '@mui/material';
+import { IconButton } from '@mui/material';
+import HeartIcon from './HeartIcon';
+import TrashIcon from './TrashIcon';
 
 function SimilarUsers() {
     const [usersWithCompatibility, setUsersWithCompatibility] = useState([]);
@@ -30,6 +34,28 @@ function SimilarUsers() {
 
     return (
         <div>
+             <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',     // added to stack items vertically
+          justifyContent: 'center',    // center items vertically
+          alignItems: 'stretch',        // center items horizontally
+          height: '100vh',             // full viewport height
+          //padding: '0 1rem',
+          position: 'relative',        // relative positioning context for the buttons
+        }}
+      >
+          <IconButton 
+          aria-label="dislike" 
+          sx={{ 
+            position: 'absolute',          // absolute positioning
+            top: '59%',                    // half the height of the viewport
+            left: '13.5rem',                  // 1rem from the left edge
+            transform: 'translateY(-50%)'  // vertical centering
+          }}
+        >
+          <TrashIcon height="115rem" width="115rem"/>
+        </IconButton>
             {usersWithCompatibility.length > 0 && (
                 <>
                     <ProfileTemplate data={usersWithCompatibility[currentIndex].user} />
@@ -39,6 +65,20 @@ function SimilarUsers() {
                     </div>
                 </>
             )}
+
+<IconButton 
+          aria-label="like" 
+          sx={{ 
+            position: 'absolute',          // absolute positioning
+            top: '60%',                    // half the height of the viewport
+            right: '14rem',                  // 1rem from the left edge
+            transform: 'translateY(-50%)'  // vertical centering
+          }}
+          onClick={goToNextUser}
+        >
+          <HeartIcon height="90rem" width="90rem"/>
+        </IconButton>
+            </Box>
         </div>
     );
 }
--- src/pages/FindMatch.js
+++ src/pages/FindMatch.js
@@ -29,70 +29,13 @@ function FindMatch() {
   const {personalityScore, setPersonalityScore} = usePersonality();
 
   
-
   return (
     <motion.div
     initial={{opacity: 0}}
     animate={{opacity: 1}}
     exit={{opacity: 0, transition: {duration: 0.4}}}>
       <MainNavbar />
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',     // added to stack items vertically
-          justifyContent: 'center',    // center items vertically
-          alignItems: 'stretch',        // center items horizontally
-          height: '100vh',             // full viewport height
-          //padding: '0 1rem',
-          position: 'relative',        // relative positioning context for the buttons
-        }}
-      >
         <SimilarUsers />
-        {/* <IconButton 
-          aria-label="dislike" 
-          sx={{ 
-            position: 'absolute',          // absolute positioning
-            top: '60%',                    // half the height of the viewport
-            left: '1rem',                  // 1rem from the left edge
-            transform: 'translateY(-50%)'  // vertical centering
-          }}
-        >
-          <DeleteForeverIcon style={{ fontSize: '6rem' }}/>
-        </IconButton>
-        <IconButton 
-          aria-label="like" 
-          sx={{ 
-            position: 'absolute',          // absolute positioning
-            top: '60%',                    // half the height of the viewport
-            right: '1rem',                 // 1rem from the right edge
-            transform: 'translateY(-50%)'  // vertical centering
-          }}
-        >
-          <FavoriteBorderIcon style={{ fontSize: '6rem' }}/>
-        </IconButton> */}
-        <IconButton 
-          aria-label="dislike" 
-          sx={{ 
-            position: 'absolute',          // absolute positioning
-            top: '59%',                    // half the height of the viewport
-            left: '13.5rem',                  // 1rem from the left edge
-            transform: 'translateY(-50%)'  // vertical centering
-          }}
-        >
-          <TrashIcon height="115rem" width="115rem"/>
-        </IconButton>
-        <IconButton 
-          aria-label="like" 
-          sx={{ 
-            position: 'absolute',          // absolute positioning
-            top: '60%',                    // half the height of the viewport
-            right: '14rem',                  // 1rem from the left edge
-            transform: 'translateY(-50%)'  // vertical centering
-          }}
-        >
-          <HeartIcon height="90rem" width="90rem"/>
-        </IconButton>
-      </Box>
       </motion.div>
   );
 }
